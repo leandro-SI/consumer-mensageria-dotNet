9786acc1f07b5795f1f21dc7c4fec3420f14bedc
Leandro <leandro.almeida@uvvnet.com.br>
2023-03-26 14:00:08 -0300

    api para receber as mensagens

---
+++ ConsumerMensageria/Controllers/MessagesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text.Json;
+using System.Text;
+
+namespace ConsumerMensageria.Controllers
+{
+
+    [Route("api/messages")]
+    public class MessagesController : ControllerBase
+    {
+        private const string QUEUE_NAME = "messages";
+        private readonly ConnectionFactory _factory;
+
+        public MessagesController()
+        {
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+        }
+
+        [HttpGet]
+        public IActionResult GetMessage()
+        {
+
+            using (var connection = _factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    // Declara a fila a partir da qual irei consumir as mensagens.
+                    channel.QueueDeclare(
+                            queue: QUEUE_NAME,
+                            durable: false,
+                            exclusive: false,
+                            autoDelete: false,
+                            arguments: null
+                            );
+
+                }
+            }
+
+            return Accepted();
+        }
+
+    }
+}
